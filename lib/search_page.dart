773fc38d0b83a36de4c0945b9b10ff6d45f74456
nect2r <nect2r@kakao.com>
2021-04-30 23:26:26 +0900

    load Firestore search_page.dart

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/create_page.dart';
@@ -29,22 +30,35 @@ class _SearchPageState extends State<SearchPage> {
     );
   }
 
-  _bulidBody() {
-    return GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          childAspectRatio: 1.0,
-          mainAxisSpacing: 1.0,
-          crossAxisSpacing: 1.0),
-        itemCount: 5,
-        itemBuilder: (context, index) {
-          return _bulidListItem(context, index);
+  Widget _bulidBody() {
+    return StreamBuilder(
+      stream: Firestore.instance.collection('post').snapshots(),
+      // ignore: missing_return
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator());
         }
+
+        var items = snapshot.data.documents ?? [];
+
+        return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                childAspectRatio: 1.0,
+                mainAxisSpacing: 1.0,
+                crossAxisSpacing: 1.0),
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              return _bulidListItem(context, items[index]);
+            }
+        );
+      },
     );
   }
 
-  _bulidListItem(BuildContext context, int index) {
-    return Image.network('https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/p1080x1080/164707897_757268328491810_4756913747223759001_n.jpg?tp=1&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=1&_nc_ohc=WDfTtIYlFdsAX_W2_MX&ccb=7-4&oh=ed5b9ada54dbb0cee0e8a259bbb6f5b1&oe=6088E8E8&_nc_sid=4f375e',
+  _bulidListItem(context, document) {
+    return Image.network(
+      document['photoUrl'],
       fit: BoxFit.cover,
     );
   }
